58d0c5aacdec294853716ec0868b95b939f21605
ignism <markbrand@Marks-iMac.fritz.box>
2019-05-03 14:39:02 +0200

    Initial push

---
+++ config/theme-config.js
@@ -0,0 +1,126 @@
+const slug = require('slug')
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let config = {
+  name: 'Timber Tailwind',
+  slug: 'timber-tailwind',
+  class: 'TimberTailwind',
+  description: 'A Timber wordpress theme using tailwindcss.',
+  author: {
+    name: 'Anonymous',
+    email: 'demo@anonymous.com'
+  }
+}
+
+readline.question('Theme name <Timber Tailwind>? ', (name) => {
+  name = name.toString().trim()
+
+  if (name.length) {
+    config.name = name
+    config.slug = getSlug(name)
+    config.class = getClass(name)
+  }
+
+  readline.question('Short description <A Timber wordpress theme using tailwindcss.>? ', (description) => {
+    description = description.toString().trim()
+
+    if (description.length) {
+      config.description = description
+    }
+
+    readline.question('Your name <Anonymous>? ', (name) => {
+      name = name.toString().trim()
+
+      if (name.length) {
+        config.author.name = name
+      }
+
+      readline.question('Your email <demo@anonymous.com>? ', (email) => {
+        email = email.toString().trim()
+
+        if (email.length) {
+          config.author.email = email
+        }
+
+        doGenerate()
+
+        readline.close()
+      })
+    })
+  })
+})
+
+function getSlug (name) {
+  let options = {
+    replacement: '-',
+    symbols: true,
+    remove: /[.]/g,
+    lower: true
+  }
+
+  return slug(name, options)
+}
+
+function getClass (name) {
+  let parts = getSlug(name).split('-')
+  let className = ''
+
+  parts.forEach((part) => {
+    className += part.charAt(0).toUpperCase() + part.substring(1)
+  })
+
+  return className
+}
+
+function doGenerate () {
+  writeConfigFile()
+
+  generateStyleCSS()
+  generateFunctionsPhp()
+}
+
+function generateStyleCSS () {
+  let data = '/*\n  Theme Name: ' + config.name + '\n  Version: 0.1.0\n  Author: ' + config.author.name + '\n  Description: A Timber wordpress theme using tailwindcss.\n  */\n\n@import "./css/theme.css"'
+
+  let file = 'style.css'
+
+  fs.writeFile(path.resolve(__dirname, '../theme/public/', file), data, (err) => {
+    if (err)
+      console.log(err)
+  })
+}
+
+function generateFunctionsPhp () {
+  let data
+
+  let file = 'functions.php'
+
+  fs.readFile(path.resolve(__dirname, file), 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      result = result.toString()
+      data = result.replace(/BoilerplateClass/g, config.class)
+
+      fs.writeFile(path.resolve(__dirname, '../theme/public/', file), data, (err) => {
+        if (err)
+          console.log(err)
+      })
+    }
+  })
+}
+
+function writeConfigFile () {
+  let data = JSON.stringify(config)
+
+  let file = 'theme.json'
+  fs.writeFile(path.resolve(__dirname, file), data, (err) => {
+    if (err)
+      console.log(err)
+  })
+}
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const config = require(path.resolve(__dirname, 'config/theme.json'))
+
+
+
+module.exports = (env, options) => {
+  console.log('env: ')
+  console.log(env)
+  console.log('options: ')
+  console.log(options)
+
+  return ({
+    entry: { 
+      theme: './theme/src/index.js' 
+    },
+    output: {
+      path: path.resolve(__dirname, 'wp-content/themes/', config.slug),
+      filename: 'js/[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  ctx: {
+                    purgecss: options.mode === 'production' ? { content: ['./**/*.twig'] } : false,
+                    cssnano: options.mode === 'production' ? {} : false
+                  }
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+        chunkFilename: 'css/[id].css'
+      }),
+      new CleanWebpackPlugin(path.resolve(__dirname, 'wp-content/themes/', config.slug)),
+      new CopyWebpackPlugin([
+        {from: 'theme/public', to: ''},
+        {from: 'theme/vendor', to: 'vendor'}
+      ]),
+    ]
+  })
+}
