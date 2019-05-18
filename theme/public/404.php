f5dacaae3463af60e8c106e98f1b057e5d057d70
Mark Brand <markbrand@Marks-MBP-2.fritz.box>
2019-05-19 00:49:14 +0200

    added 404 page

---
+++ theme/public/404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: 404
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+$context = Timber::context();
+
+Timber::render('pages/404.html.twig', $context);
